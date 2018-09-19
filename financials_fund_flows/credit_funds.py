6c036596b560a8d4d05704a70ebac116fd6574cb
Dustin Sugasa <dustin.sugasa@gmail.com>
2018-09-19 23:03:17 +0100

    making some progress

--- financials_fund_flows/credit_funds.py
+++ financials_fund_flows/credit_funds.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 19 16:40:07 2018
-
-@author: dpsugasa
-"""
-
-"""
 Created on Thu Dec 15 15:18:02 2016
 
 Flow of funds analysis; starting with credit
@@ -17,6 +11,7 @@ import pandas as pd
 from tia.bbg import LocalTerminal
 import numpy as np
 import matplotlib as plt
+from collections import OrderedDict
 from datetime import datetime
 from operator import itemgetter
 import plotly
@@ -35,23 +30,43 @@ start_date = '01/01/2012'
 end_date = "{:%m/%d/%Y}".format(datetime.now())
 
 #read fund names from .csv
-oe = pd.read_csv(r'C:\Users\dpsugasa\WorkFiles\fund_flows\financials_fund_flows\open_end_all_curr.csv',
+oe = pd.read_csv(r'C:\Users\dpsugasa\WorkFiles\fund_flows\financials_fund_flows\open_end_all_curr_mini.csv',
                    parse_dates=True, infer_datetime_format=True)
 oe = oe['Ticker'].values.tolist()
- 
-etf = 4                
-assets = ['open_end']
-fields = ['FUND_TOTAL_ASSETS'] #'FUND_TOTAL_ASSETS_CRNCY']
+
+fx = ['EURUSD Curncy', 'GBPUSD Curncy']
+
+q = {'open_end': oe,
+     
+     }
 
 
-d = {} #dict of original dataframes per asset class
-m = {} #list of lists for data matrix
-n = {} #list of lists for data matrix 2
+fields_hist = ['FUND_TOTAL_ASSETS']
+fields_ref = ['FUND_TOTAL_ASSETS_CRNCY']
+fields_fx = ['LAST_PRICE']
 
-for i in assets:
-    d[i] = LocalTerminal.get_historical(oe, fields, start_date, end_date, period = 'DAILY',
+d_fx = LocalTerminal.get_historical(fx, fields_fx, start_date, end_date, period = 'DAILY',
                                          non_trading_day_fill_option = 'ALL_CALENDAR_DAYS',
                                          non_trading_day_fill_method = 'PREVIOUS_VALUE').as_frame()
-    d[nam].columns = d[name].columns.droplevel()
-                                   #non_trading_day_fill_option = days,
-                                   #non_trading_day_fill_method = fill)
+d_fx.columns = d_fx.columns.droplevel(-1)
+d = {} #dict of original dataframes per asset class
+m = {} #dict of ref data
+n = {} #list of lists for data matrix 2
+
+for i, v in q.items():
+    d[i] = LocalTerminal.get_historical(v, fields_hist, start_date, end_date, period = 'DAILY',
+                                         non_trading_day_fill_option = 'ALL_CALENDAR_DAYS',
+                                         non_trading_day_fill_method = 'PREVIOUS_VALUE').as_frame()
+    
+    d[i].columns = d[i].columns.droplevel(-1)
+
+    
+    m[i] = LocalTerminal.get_reference_data(v,fields_ref).as_frame()
+    
+    
+    
+    
+    d[i].columns = d[i].columns.droplevel()
+    
+print ("Time to complete:", datetime.now() - start_time)
+                                   
