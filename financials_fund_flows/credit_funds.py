6a474400fa3e5f154abd0b10a387b12314c9f325
Unknown <dustin.sugasa@gmail.com>
2018-09-24 23:21:49 +0100

    completed the $Assets lookup; struggled to get it right

--- financials_fund_flows/credit_funds.py
+++ financials_fund_flows/credit_funds.py
@@ -49,24 +49,70 @@ d_fx = LocalTerminal.get_historical(fx, fields_fx, start_date, end_date, period 
                                          non_trading_day_fill_option = 'ALL_CALENDAR_DAYS',
                                          non_trading_day_fill_method = 'PREVIOUS_VALUE').as_frame()
 d_fx.columns = d_fx.columns.droplevel(-1)
+d_fx = d_fx.rename(columns = {'EURUSD Curncy':'EUR',
+                                      'GBPUSD Curncy':'GBP'})
+d_fx['USD'] = 1.0
+#d_fx = d_fx.to_dict()
+
+
 d = {} #dict of original dataframes per asset class
 m = {} #dict of ref data
-n = {} #list of lists for data matrix 2
+z = {} #list of lists for data matrix 2
+idx = pd.IndexSlice
 
 for i, v in q.items():
+    #get ref data and underlying currency
+    m[i] = LocalTerminal.get_reference_data(v,fields_ref).as_frame()
+    #get asset data and calculate $Assets on a daily basis
     d[i] = LocalTerminal.get_historical(v, fields_hist, start_date, end_date, period = 'DAILY',
                                          non_trading_day_fill_option = 'ALL_CALENDAR_DAYS',
                                          non_trading_day_fill_method = 'PREVIOUS_VALUE').as_frame()
-    
     d[i].columns = d[i].columns.droplevel(-1)
 
+    d[i] = d[i].unstack().to_frame()
+    d[i].columns = d[i].columns.astype(str)
+    d[i].columns = d[i].columns.str.replace('0','Assets')
+    d[i]['fx'] = m[i]['FUND_TOTAL_ASSETS_CRNCY'].loc[d[i].index.get_level_values(0)].values
+    d[i]['rate'] = d_fx.lookup(d[i].index.get_level_values(1), d[i]['fx'])
+    d[i]['$Assets'] = d[i]['rate']*d[i]['Assets']
     
-    m[i] = LocalTerminal.get_reference_data(v,fields_ref).as_frame()
+    
+    #d[i].columns = d[i].columns.droplevel(-1)
     
     
     
     
-    d[i].columns = d[i].columns.droplevel()
     
+#    d[i].columns = d[i].columns.droplevel(-1)
+#    d[i].loc[idx[:,:] 'Curr'] = m[i].xs(d[i].loc[idx[:]], axis=1)
+#    
+#    
+#    
+#    d[i] = d[i].unstack().to_frame()
+#    d[i].columns = d[i].columns.droplevel(-1)
+#    
+#    
+#    
+#    d[i] = d[i].unstack().to_frame()
+#    d[i].columns = d[i].columns.astype(str)
+#    d[i].columns = d[i].columns.str.replace('0','Assets')
+#    #ticker = d[i].index.get_level_values(0)
+#    #d[i].loc[idx[:,:],'$Assets'] = 1
+#    d[i]['booty'] = m[i]['FUND_TOTAL_ASSETS_CRNCY']
+    
+    #d[i].loc[idx[:,:], 'nutgargler'] = d[i].xs( level=0, axis = 0)
+    #z[i] = d[i].rename(index = m[i]['FUND_TOTAL_ASSETS_CRNCY'], level=0)
+    #z[i] = d[i].set_index(m[i]['FUND_TOTAL_ASSETS_CRNCY'], append=True, inplace=False)#d[i].loc[idx[:,:], 'suckit'] = m[i].loc[d[i].index.get_level_values(0)].values
+    #d[i].loc[idx[:,:], 'balls'] = pd.to_datetime(d[i].index.get_level_values(1))
+    #d[i].loc[idx[:,:], 'gargle'] = d_fx.get_value(d[i]['balls'], d[i]['suckit'])
+    
+    
+    #d_fx.loc[idx[d[i]['balls'],d[i]['suckit']]]                
+    
+    
+    
+    
+        
 print ("Time to complete:", datetime.now() - start_time)
-                                   
+
+
