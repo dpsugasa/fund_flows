ae9d6a773e108268a11d51f1a1e8f52b01e16840
Dustin Sugasa <dustin.sugasa@gmail.com>
2018-09-27 14:17:52 +0100

    quite a few changes to the data resampling

--- financials_fund_flows/credit_funds.py
+++ financials_fund_flows/credit_funds.py
@@ -37,6 +37,7 @@ oe = oe['Ticker'].values.tolist()
 fx = ['EURUSD Curncy', 'GBPUSD Curncy']
 
 q = {'open_end': oe,
+     'booty': oe
      
      }
 
@@ -56,7 +57,16 @@ d_fx['USD'] = 1.0
 
 d = {} #dict of original dataframes per asset class
 m = {} #dict of ref data
-b = {} #list of lists for data matrix 2
+b = {} #list of lists for asset diffs
+p = {} #list of list for $asset diffs
+f = {} #simplified asset dicts
+r = {} #daily rate of change dict
+u = {} #weekly rate of change
+pq = {} #monthly rate of change
+ip = {} #quarterly rate of change
+lp = {} #yearly rate of change
+
+
 idx = pd.IndexSlice
 
 for i, v in q.items():
@@ -74,49 +84,46 @@ for i, v in q.items():
     d[i]['fx'] = m[i]['FUND_TOTAL_ASSETS_CRNCY'].loc[d[i].index.get_level_values(0)].values
     d[i]['rate'] = d_fx.lookup(d[i].index.get_level_values(1), d[i]['fx'])
     d[i]['$Assets'] = d[i]['rate']*d[i]['Assets']
+    
+    f[i] = d[i].unstack(0)
+    f[i] = f[i].swaplevel(1,0,axis=1)
+    f[i] = f[i].drop(['fx', 'rate'], axis=1, level=1)
+    f[i] = f[i].stack(0)
+        
     for z in range(1,11):
-        d[i][f'Loc_Diff-{z}'] = d[i]['Assets'].diff(z)
-        d[i][f'$_Diff-{z}'] = d[i]['$Assets'].diff(z)
-        b[f'tot_diff{z}'] = d[i][f'$_Diff-{z}'].sum()
-
+        b[f'assets_diff_{z}'] = f[i]['Assets'].unstack().diff(z)
+        p[f'$assets_diff_{z}'] = f[i]['$Assets'].unstack().diff(z)
+        f[i][f'Diff_{z}'] = b[f'assets_diff_{z}'].stack()
+        f[i][f'$Diff_{z}'] = p[f'$assets_diff_{z}'].stack()
+        
+    r[i] = f[i][['$Diff_1']].unstack() #.swaplevel(1,0, axis=1)
+    r[i] = r[i].swaplevel(0,1,axis=1)
+    r[i].columns = r[i].columns.droplevel(-1)
+    r[i] = r[i].fillna(0).apply(sum, axis=1).to_frame()
+    r[i].columns = ['total_diff']
+    r[i]['cum_diff'] = r[i]['total_diff'].cumsum()
+    
+    #create weekly changes
+    u[i] = r[i]['total_diff'].resample('W').sum().to_frame()
+    u[i].columns = ['tot_diff_wkly']
+    u[i]['cum_chg_wkly'] = u[i]['tot_diff_wkly'].cumsum()
+    
+    #create monthly changes
+    pq[i] = r[i]['total_diff'].resample('BM').sum().to_frame()
+    pq[i].columns = ['tot_diff_mthly']
+    pq[i]['cum_chg_mthly'] = pq[i]['tot_diff_mthly'].cumsum()
+    
+    #create monthly changes
+    ip[i] = r[i]['total_diff'].resample('BQ').sum().to_frame()
+    ip[i].columns = ['tot_diff_qtrly']
+    ip[i]['cum_chg_qtrly'] = ip[i]['tot_diff_qtrly'].cumsum()
     
     
-    
-    #d[i].columns = d[i].columns.droplevel(-1)
-    
-    
-    
-    
-    
-#    d[i].columns = d[i].columns.droplevel(-1)
-#    d[i].loc[idx[:,:] 'Curr'] = m[i].xs(d[i].loc[idx[:]], axis=1)
-#    
-#    
-#    
-#    d[i] = d[i].unstack().to_frame()
-#    d[i].columns = d[i].columns.droplevel(-1)
-#    
-#    
-#    
-#    d[i] = d[i].unstack().to_frame()
-#    d[i].columns = d[i].columns.astype(str)
-#    d[i].columns = d[i].columns.str.replace('0','Assets')
-#    #ticker = d[i].index.get_level_values(0)
-#    #d[i].loc[idx[:,:],'$Assets'] = 1
-#    d[i]['booty'] = m[i]['FUND_TOTAL_ASSETS_CRNCY']
-    
-    #d[i].loc[idx[:,:], 'nutgargler'] = d[i].xs( level=0, axis = 0)
-    #z[i] = d[i].rename(index = m[i]['FUND_TOTAL_ASSETS_CRNCY'], level=0)
-    #z[i] = d[i].set_index(m[i]['FUND_TOTAL_ASSETS_CRNCY'], append=True, inplace=False)#d[i].loc[idx[:,:], 'suckit'] = m[i].loc[d[i].index.get_level_values(0)].values
-    #d[i].loc[idx[:,:], 'balls'] = pd.to_datetime(d[i].index.get_level_values(1))
-    #d[i].loc[idx[:,:], 'gargle'] = d_fx.get_value(d[i]['balls'], d[i]['suckit'])
-    
-    
-    #d_fx.loc[idx[d[i]['balls'],d[i]['suckit']]]                
-    
-    
-    
-    
+    #create business yearly changes
+    lp[i] = r[i]['total_diff'].resample('BY').sum().to_frame()
+    lp[i].columns = ['tot_diff_yearly']
+    lp[i]['cum_chg_yearly'] = lp[i]['tot_diff_yearly'].cumsum()
+                 
         
 print ("Time to complete:", datetime.now() - start_time)
 
